c77876e0c511e5eb352db489c12bfd5e4e1554d8
Bello2288 <bellofattogregg@gmail.com>
2022-10-19 21:54:06 -0400

    fixed wwordle play again, now when playing again it will select a new word

--- wordle.py
+++ wordle.py
@@ -14,8 +14,8 @@ def print_menu():
        '--'   '--' `---' `--'    `---' `--' `----'                                                                                                                                                                
     ''')
     print(Fore.BLUE + "{}" * 30)
-    print("\n\n")
-    print("Enter a five letter word. You get 6 guesses.\n")
+    print("\n")
+    print("  Enter a five letter word. You get 6 guesses.\n\n")
 
 def read_random_word():
     with open("words.txt") as f:
@@ -23,34 +23,37 @@ def read_random_word():
         return random.choice(words)
 
 
-word = read_random_word()
+# word = read_random_word()
 play_again = ""
 
 while play_again != "n":
     print_menu()
+    word = read_random_word()
     for attempt in range(1, 7):
-        guess = input().lower()
-        sys.stdout.write('\x1b[1A')
-        sys.stdout.write('\x1b[2K')
+        guess = input("  Enter your word:   ").lower()
+        print()
+        # sys.stdout.write('\x1b[1A')
+        # sys.stdout.write('\x1b[2K')
 
         for i in range( min(len(guess), 5)):
             if guess[i] == word[i]:
                 print(Back.GREEN + guess[i], end="")
             elif guess[i] in word:
-                print(Back.YELLOW + guess[i], end="")
+                print(Back.LIGHTYELLOW_EX + guess[i], end="")
             else:
                 print(guess[i], end="")
-        print("\n")
+        print("\n   ")
         print(Fore.BLUE + "{}" * 30)
+        print("\n   ")
 
         if guess == word:
-            print(Fore.LIGHTGREEN_EX + "\nYou guessed the word in", attempt, Fore.CYAN + "tries!")
+            print(Fore.LIGHTGREEN_EX + "\n  You guessed the word in", attempt, Fore.LIGHTGREEN_EX + "tries!")
             break
 
         elif attempt == 6:
-            print(Fore.RED + "\nYou ran out of guesses! The word was", word, Fore.RED + ". Better luck next time...")
+            print(Fore.RED + "\n  You ran out of guesses! The word was", word, Fore.RED + ". Better luck next time...")
 
-    play_again = input(Fore.YELLOW + "\nEnter 'n' to exit or enter any other key to play again. ")
+    play_again = input(Fore.LIGHTYELLOW_EX + "\n  Enter 'n' to exit or enter any other key to play again. ")
     if play_again == "n":
-        print(Fore.MAGENTA + "\n\nThanks for playing!\n\n\n\n\n")
+        print(Fore.LIGHTGREEN_EX + "\n\n  Thanks for playing!\n\n\n\n\n")
         break
